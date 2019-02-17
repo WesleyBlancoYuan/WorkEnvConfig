6b7643276c8378a4bde7c29017a8b261c52b600b
WesternGun <westernguncyber@gmail.com>
2019-02-17 13:21:42 +0100

    add putty export settings script, and an updated example of settings with basic values set.

---
+++ putty/export-putty.bat
@@ -0,0 +1,3 @@
+:: export putty x32 all settings, without elevated privilege
+reg export HKEY_CURRENT_USER\SOFTWARE\SimonTatham %USERPROFILE%\putty.reg
+
---
+++ putty/export-puttyx64.bat
@@ -0,0 +1,3 @@
+:: export putty x64 all settings, without elevated privilege
+reg export HKEY_LOCAL_MACHINE\SOFTWARE\SimonTatham %USERPROFILE%\puttyx64.reg
+
